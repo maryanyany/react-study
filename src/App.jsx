8f0061b7b323cd0e475d7f88c96ea90ca7ac75bf
maryanyany <maryanebezeraoliveira@gmail.com>
2024-09-14 13:37:28 -0300

    Feat: chamando o accordion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Count from "./class/hooks/useState/count";
+import Accordion from "./class/hooks/useState/accordion";
 
 export default function App(){
   return(
-    <div
-    className="app-container"> <Count/> </div>
+    // <div className="app-container"> 
+      <Accordion/>
+    // </div>
 
   )
 }
+
+// import Count from "./class/hooks/useState/count";
+
+// export default function App(){
+//   return(
+//     <div className="app-container"> 
+//       <Count/> 
+//     </div>
+
+//   )
+// }
 
 // import Hero from './components/hero/'
 
